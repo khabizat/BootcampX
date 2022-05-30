18fb7069faef66b57aa60c1ee6e057cbb3b5be79
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-30 23:03:38 +0000

    Get the total amount of time that all students from a specific cohort have spent on all assignments

---
+++ 2_queries_joins/2_cohort_total_assignment_duration.sql
@@ -0,0 +1,8 @@
+--Get the total amount of time that all students from a specific cohort have spent on all assignments
+SELECT SUM(assignment_submissions.duration) as total_duration
+FROM assignment_submissions
+JOIN students 
+ON students.id = assignment_submissions.student_id
+JOIN cohorts
+ON cohorts.id = students.cohort_id
+WHERE cohorts.name = 'FEB12';
