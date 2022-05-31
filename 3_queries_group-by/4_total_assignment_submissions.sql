a1026ede041fbb3ae357320186993bfe2abe67c8
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-31 00:36:54 +0000

    Get the total number of assignment submissions for each cohort

---
+++ 3_queries_group-by/4_total_assignment_submissions.sql
@@ -0,0 +1,9 @@
+-- Get the total number of assignment submissions for each cohort
+SELECT cohorts.name, count(assignment_submissions.*) as total_submissions
+FROM cohorts
+JOIN students
+ON cohorts.id = students.cohort_id
+JOIN assignment_submissions
+ON students.id = assignment_submissions.student_id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
