2f38c051ca5db50307b9f185ccacb3730ffdfc74
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-31 19:46:36 +0000

    Refactor

--- 4_queries/4_average_assistance_time.sql
+++ 4_queries/4_average_assistance_time.sql
@@ -1,3 +1,3 @@
 -- Get the average time of an assistance request
-SELECT avg(assistance_requests.completed_at - assistance_requests.started_at) as average_assistance_request_duration
+SELECT avg(completed_at - started_at) as average_assistance_request_duration
 FROM assistance_requests;
