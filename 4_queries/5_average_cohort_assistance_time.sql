4790ba374873312a3370f36925c5882d686d7aa4
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-31 19:59:33 +0000

    Get the average duration of assistance requests for each cohort

---
+++ 4_queries/5_average_cohort_assistance_time.sql
@@ -0,0 +1,7 @@
+-- Get the average duration of assistance requests for each cohort
+SELECT cohorts.name, avg(assistance_requests.completed_at - assistance_requests.started_at) as average_assistance_time
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time ASC;
