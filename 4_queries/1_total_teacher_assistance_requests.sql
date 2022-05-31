42d5dc47b796927133b2790233950ec0219084be
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-31 19:12:24 +0000

    Get the total number of assistance_requests for a teacher

---
+++ 4_queries/1_total_teacher_assistance_requests.sql
@@ -0,0 +1,7 @@
+-- Get the total number of assistance_requests for a teacher
+SELECT teachers.name, count(assistance_requests.*) as total_assistances
+FROM teachers
+JOIN assistance_requests
+ON teachers.id = teacher_id
+WHERE teachers.name = 'Waylon Boehm'
+GROUP BY teachers.name;
