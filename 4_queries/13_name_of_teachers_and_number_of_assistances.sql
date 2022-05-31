04b775f666620d839b4d20af5aa77b23ffec467e
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-31 22:00:05 +0000

    All teachers that performed an assistance request during a cohort with # of assistances

---
+++ 4_queries/13_name_of_teachers_and_number_of_assistances.sql
@@ -0,0 +1,10 @@
+-- Get the name of all teachers that performed an assistance request during a cohort, and include the number of assistances as well
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
+GROUP BY teachers.name, cohorts.name
+ORDER BY teacher;
+
