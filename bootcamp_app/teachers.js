c96ed38cf3cf7a9bad2e488d56ca7a4f3c205834
Khabizat Sharapudinova <habizat22@gmail.com>
2022-06-02 04:29:02 +0000

    Sanitize input for parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,18 +7,26 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id  
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.teacher} assisted in the ${user.cohort} cohort`);
-  })
-  pool.end();
-}).catch(err => console.error('query error', err.stack));
+const queryString = `
+  SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+  FROM teachers
+  JOIN assistance_requests ON teacher_id = teachers.id
+  JOIN students ON student_id = students.id
+  JOIN cohorts ON cohort_id = cohorts.id  
+  WHERE cohorts.name LIKE $1
+  ORDER BY teacher
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool
+  .query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.teacher} assisted in the ${user.cohort} cohort`);
+    });
+    pool.end();
+  }).catch(err => console.error('query error', err.stack));
